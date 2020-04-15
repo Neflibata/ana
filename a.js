1f8709d20498303b265f19d99b3ef05c61d5312b
tan-zhi-wen <qwe96082051@qq.com>
2020-04-15 22:02:07 +0800

    a

--- a.js
+++ a.js
@@ -1,15 +1,12 @@
-function test(){
+﻿function test(){
     log.console('ssss')
 }
 function test2(){
         console.log('第二次提交')
 }
 function test3(){
-<<<<<<< HEAD
-    console.log("第三次提交")
-=======
-    console.log("第三ss次提交")
->>>>>>> 0fdde6e... 全部提交
+   console.log("第三次提交")
+
 }
 function test4(){
     console.log("第4次提交")
--- b.js
+++ b.js
@@ -1,1 +1,1 @@
-self commit
+self commitccvs
